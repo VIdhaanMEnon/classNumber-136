068f1d1ee8e15dcffce93002eb179cddd7a3ba77
VIdhaanMEnon <89589933+VIdhaanMEnon@users.noreply.github.com>
2022-02-09 14:45:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = "";
 Status = "";
+objects = [];
 
 function preload(){
     video = createVideo('video.mp4');
@@ -13,6 +14,23 @@ function setup(){
 
 function draw(){
     image(video , 0 , 0 , 480 , 380);
+
+    if(Status != ""){
+        objectDetector.detect(video , gotResults);
+
+        
+    }
+
+    for(i = 0; i<objects.length; i++){
+        document.getElementById("status").innerHTML = "status: objects detected";
+        document.getElementById("number_of_objects").innerHTML = "number of objects detected are: " + objects.length;
+        fill("red");
+        percent = floor(objects[i].confidence*100);
+        text(objects[i].label + " " + percent + "%" , objects[i].x , objects[i].y);
+        noFill()
+        stroke("red");
+        rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height)
+    }
 }
 
 function start(){
@@ -27,3 +45,16 @@ function modelLoaded(){
     video.speed(1);
     video.volume(0.5);
 }
+
+function gotResults(error , results){
+    if(error ){
+        console.log(error);
+    }
+
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+
